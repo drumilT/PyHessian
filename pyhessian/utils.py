83169aa2b75d9227a44efc99dbb907e624eb89d9
Drumil Trivedi <31318480+drumilT@users.noreply.github.com>
2020-01-03 10:31:28 +0100

    Update utils.py
    consider only weight params

--- pyhessian/utils.py
+++ pyhessian/utils.py
@@ -64,8 +64,10 @@ def get_params_grad(model):
     """
     params = []
     grads = []
-    for param in model.parameters():
+    for name, param in model.named_parameters():
         if not param.requires_grad:
+            continue
+        if 'bias' in name:
             continue
         params.append(param)
         grads.append(0. if param.grad is None else param.grad + 0.)
